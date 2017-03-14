a679945f3f1c283ae1e9aef1ff25d42126456c55
Edward <edward940811@yahoo.com.tw>
2017-03-14 19:48:11 +0800

    new post function added

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,30 @@
 class TopicsController < ApplicationController
 
-  # def index
-  #   @topics = Topic.all
-  # end
+  def index
+   @topics = Topic.all
+  end
 
+  def manageposts
+    @topics = Topic.all
+  end
+
+  def create
+    @topic = Topic.create(topic_params)
+
+   if @topic.save
+     redirect_to manageposts_path
+   else
+     render :new
+   end
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+private
+
+  def topic_params
+        params.require(:topic).permit(:title, :description)
+  end
 end
